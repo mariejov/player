d2d1ad5d7a3cf0f58f9f9e09e073bd1ea4054afc
mariejov <marija.mhh@gmail.com>
2017-10-01 01:34:44 +0200

    my music

---
+++ music.js
@@ -0,0 +1,16 @@
+var xml='<?xml version = "1.0"?>';
+xml +='<music>';
+xml +='<composition>';
+xml +='		<title>song.mp3</title>';
+xml +='<composer>Kompozitor 1</composer>';
+xml +='  <time>4:05</time>';
+xml +='	 <filename>Pema broj 1</filename>';
+xml +='</composition>';
+	
+xml +='<composition>';
+xml +='  <title>song1.mp3</title>';
+xml +='<composer>Kompozitor 2</composer>';
+xml +='  <time>3:55</time>';
+xml +='  <filename>Pesma broj 2</filename>';
+xml +='</composition>';
+xml +='</music>';
